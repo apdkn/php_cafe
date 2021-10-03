b54606dcd29827a19fda59990422d7a5b3dcb34f
taemoon <ba178711@gmail.com>
2021-10-03 13:50:40 +0900

    server

--- q2.php
+++ q2.php
@@ -1,34 +1,26 @@
+<?php header("Content-Type:text/html;charset=utf-8");
+  echo "<body style='background-color:#E6B36D'>";
+ ?>
+ <!DOCTYPE html>
+ <html>
+ <body>
+ <?php
+ echo "
+ <h1 style=\"color: black;margin-left: 870px;font-size: 20px;;\">주문서<br></h>
+ ";
+
+ echo "
+ <h2 style=\"color: black;margin-left: 870px;font-size: 20px;\"><br></h>
+ ";
+ ?>
+ </body>
+ </html>
+
 <?php
-// <link rel=stylesheet href='style.css' type='text/css'>
-
-echo "주문서<br>";
-
-// $num1 = count($_POST["num"]);
-// echo "주문 개수 : ".$_POST["num"]."<br>";
-// for ($q=0; $q<$num1; $q++) {
-//   echo $_POST["num"][$q];
-//   if($q != $num1 - 1)
-//     echo ", ";
-// }
-//
-// echo "사이즈 : ".$_POST["usize"]."<br>";
-// echo "결제금액 : ".$_POST["price"]."<br>";
-
-
-
-// switch (variable) {
-//   case 'value':
-//     // code...
-//     break;
-//
-//   default:
-//     // code...
-//     break;
-// }
-
 // function
 //cafe menu and 개수
 //isset = 변수가 선언되었고 다른지 확인
+
 $num = count($_POST["tea"]);
 echo "주문메뉴 : ";
 for ($i=0; $i<$num; $i++) {
@@ -37,6 +29,25 @@ for ($i=0; $i<$num; $i++) {
     echo ", ";
 }
 echo "<br>";
+
+// $size = isset($_POST['usize']) ? $_POST['usize'] : true;
+// echo "사이즈 : ";
+//    if ($size) {
+//       echo htmlentities($_POST['usize'], ENT_QUOTES, "UTF-8");
+//    } else {
+//      echo "Regular";
+//    }
+
+$size = count($_POST["usize"]);
+echo "사이즈 : ";
+for ($a=0; $a<$size; $a++) {
+  echo $_POST["usize"][$a];
+  if($a != $size - 1)
+    echo " ";
+}
+echo "<br>";
+
+
 $option = isset($_POST['num']) ? $_POST['num'] : true;
 echo "아메리카노 주문 개수 : ";
    if ($option) {
@@ -48,7 +59,7 @@ echo "아메리카노 주문 개수 : ";
   echo "<br>";
 
   $option1 = isset($_POST['num1']) ? $_POST['num1'] : false;
-  echo "카페라떼 주문 개수 : ";
+  echo "카페모카 주문 개수 : ";
      if ($option1) {
         echo htmlentities($_POST['num1'], ENT_QUOTES, "UTF-8");
      } else {
@@ -58,7 +69,7 @@ echo "아메리카노 주문 개수 : ";
     echo "<br>";
 
     $option2 = isset($_POST['num2']) ? $_POST['num2'] : false;
-    echo "카페모카 주문 개수 : ";
+    echo "카페라떼 주문 개수 : ";
        if ($option2) {
           echo htmlentities($_POST['num2'], ENT_QUOTES, "UTF-8");
        } else {
@@ -75,9 +86,18 @@ echo "아메리카노 주문 개수 : ";
         echo "0";
 
          }
-        echo "<br>";
+         echo "<br>";
+echo "------------------------------------";
+         echo "<br>";
 
+echo "계산서";
+echo "<br>";
 //메뉴 가격
+$americano = "아메리카노 금액 : ";
+$caffemoca = "카페모카 금액 : ";
+$caffelatte = "카페라떼 금액 : ";
+$Vanilla = "바닐라라떼 금액 : ";
+$total_price = "합계 : ";
 $americanoR = 4100;
 $americanoL = 4600;
 $caffelatteR = 4600;
@@ -87,27 +107,54 @@ $caffemocaL = 6100;
 $VanillaR = 5100;
 $VanillaL = 5600;
 
-echo "결제 금액 : ";
+
+echo $americano;
 if($option == 3)
 echo $americanoR * 3;
-else if($option == 2)
+elseif($option == 2)
 echo $americanoR * 2;
-else
+elseif($option == 1)
 echo $americanoR;
+elseif($option == 0)
+echo "0";
 
 echo "<br>";
 
-echo "";
-if($option1 == 3)
+echo $caffemoca;
+if ($option1 == 3)
+echo $caffemocaR * 3;
+elseif($option1 == 2)
+echo $caffemocaR * 2;
+elseif($option1 == 1)
+echo $caffemocaR;
+elseif($option == 0)
+echo "0";
+
+echo "<br>";
+
+echo $caffelatte;
+if ($option2 == 3)
 echo $caffelatteR * 3;
-else if($option == 2)
+elseif($option2 == 2)
 echo $caffelatteR * 2;
-else
+elseif($option2 == 1)
 echo $caffelatteR;
+elseif($option2 == 0)
+echo "0";
 
 echo "<br>";
 
+echo $Vanilla;
+if ($option3 == 3)
+echo $VanillaR * 3;
+elseif($option3 == 2)
+echo $VanillaR * 2;
+elseif($option3 == 1)
+echo $VanillaR;
+elseif($option3 == 0)
+echo "0";
 
+echo "<br>";
 
 
 
